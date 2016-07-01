69b8e38db86f6d941722fda7362a9c4934013cea
Rafael Gregorut <rafael.mgregorut@gmail.com>
2016-06-30 22:30:54 -0300

    classe de task com 1 teste

---
+++ app/models/Task.java
@@ -0,0 +1,35 @@
+package models;
+
+import javax.persistence.*;
+
+import com.avaje.ebean.Model;
+
+import java.util.*;
+
+@Entity
+public class Task extends Model{
+
+	@Id
+	public Long id;
+	public String title;
+	public boolean done = false;
+	public Date dueDate;
+	@ManyToOne
+	public User assignedTo;
+	public String folder;
+	@ManyToOne
+	public Project project;
+	
+    public static Model.Finder<Long,Task> find = new Model.Finder(Long.class, Task.class);
+    
+    public static List<Task> findTodoInvolving(String user) {
+    	return find.fetch("project").where().eq("done", false).eq("project.members.email", user).findList();
+    }
+    
+    public static Task create(Task task, Long project, String folder) {
+        task.project = Project.find.ref(project);
+        task.folder = folder;
+        task.save();
+        return task;
+    }
+}
