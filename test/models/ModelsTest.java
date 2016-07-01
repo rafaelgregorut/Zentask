9c435c137f9b9297838e2ac002d040b3cb3001aa
Rafael Gregorut <rafael.mgregorut@gmail.com>
2016-06-30 22:13:33 -0300

    adicionei a classe project e um teste para ela

--- test/models/ModelsTest.java
+++ test/models/ModelsTest.java
@@ -1,10 +1,19 @@
 package models;
 
-import models.*;
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.inMemoryDatabase;
+import static play.test.Helpers.start;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import play.test.WithApplication;
-import static play.test.Helpers.*;
 
 public class ModelsTest extends WithApplication {
 
@@ -27,5 +36,18 @@ public class ModelsTest extends WithApplication {
 		assertNotNull(User.authenticate("bob@gmail.com", "123456"));
 		assertNull(User.authenticate("bob@gmail.com", "098765"));
 		assertNull(User.authenticate("test@test.com", "123456"));
+	}
+	
+	@Test
+	public void findProjectsInvolving() {
+		new User("bob@gmail.com", "Bob", "secret").save();
+        new User("jane@gmail.com", "Jane", "secret").save();
+
+        Project.create("Play 2", "play", "bob@gmail.com");
+        Project.create("Play 1", "play", "jane@gmail.com");
+        
+        List<Project> results = Project.findInvolving("bob@gmail.com");
+        assertEquals(1, results.size());
+        assertEquals("Play 2", results.get(0).name);
 	}
 }
