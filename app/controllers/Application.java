82ff101c3819516db74843ac7973d5f718fac075
Rafael Gregorut <rafael.mgregorut@gmail.com>
2016-07-20 21:44:28 -0300

    validacao do login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.Project;
 import models.Task;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
@@ -9,6 +10,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public Result index() {
+
         return ok(index.render(
         			Project.find.all(),
         			Task.find.all()
--- app/controllers/Login.java
+++ app/controllers/Login.java
@@ -1,12 +1,11 @@
 package controllers;
 
-import models.Project;
-import models.Task;
-import play.data.Form;
+import models.User;
+import play.Logger;
+import play.data.*;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
-
 
 public class Login extends Controller {
 	
@@ -18,7 +17,27 @@ public class Login extends Controller {
     }
 	
 	public Result authenticate() {
-		//TODO implementar autenticacao
+		//No Eclipse com opcao de build automatically o bind parece nao funcionar direito (Stack)
+		Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+		
+		if(loginForm.hasErrors()) {
+			//Logger.info(email+" "+password);
+			return badRequest(login.render(loginForm));
+		} else {
+			session().clear();
+			session("email",loginForm.get().email);
+			return redirect(routes.Application.index());
+		}
+		
+	}
+	
+	public String validate() {
+		if (User.authenticate(email, password) == null) {
+			//Mensagem retornada pelo validate se torna um erro global
+			return "Invalid user or password";
+		}
+		//se o validate passa, entao tenho que retornar null
 		return null;
 	}
+	
 }
