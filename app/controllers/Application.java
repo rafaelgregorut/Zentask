f60bc0d9d320f6dbf4ef0fb2a60046c0c1842fb2
Rafael Gregorut <rafael.mgregorut@gmail.com>
2016-07-26 22:35:56 -0300

    comecando com javascript. implementacao do router de javascript

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+//import play.api.mvc.*;
+//import play.api.routing.*;
 import models.Project;
 import models.Task;
 import models.User;
@@ -7,6 +9,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 import views.html.index;
+import play.Routes;
 
 public class Application extends Controller {
 
@@ -18,5 +21,20 @@ public class Application extends Controller {
         			User.find.byId(request().username())
         		));
     }
+	
+	//JavaScript router instead of AJAX calls with hardcoded URLs
+	public Result javascriptRoutes() {
+		//script tag
+		response().setContentType("text/javascript");
+		//Router will be bound to global variable called jsRoutes
+		return ok(
+				Routes.javascriptRouter("jsRoutes",
+						controllers.routes.javascript.ProjectController.addProject(),
+						controllers.routes.javascript.ProjectController.delete(),
+						controllers.routes.javascript.ProjectController.rename(),
+						controllers.routes.javascript.ProjectController.addFolder()
+						)
+				);
+	}
 
 }
